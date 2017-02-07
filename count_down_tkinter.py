fc74f93615e5569465a4f81df751fb1469c6d6ca
David Ton-Lai <dtonlai@ualberta.ca>
2017-02-07 15:13:11 +0900

    It's working

--- count_down_tkinter.py
+++ count_down_tkinter.py
@@ -1,32 +1,59 @@
 import tkinter as tk
-import time
-from PIL import Image, ImageTk
-
-window = tk.Tk()
-window.geometry("250x250")
-
-#message = tk.Message(window, text="this is a message", width=50)
-#message.pack
-
-def count_down():
-	timer = 10
-	text_display = tk.Text(master=window, height=5, width=10)
-	text_display.grid(row=2)
-	text_display.insert(tk.END, timer)
-	while timer >= 0:
-		text_display.configure(text=timer)
-		time.sleep(1)
-		timer -= 1
-	time.sleep(1)
-	timer = "Ka-Boom!"
-#def configure_label():
-	#label.configure(text="Button clicked!")
 
 
-label = tk.Label(window, text="Don't click that button!", width=200)
-label.pack()
+root = tk.Tk()
 
-button = tk.Button(window, text="Start" , command=count_down)
-button.pack()
+###Variables
+timer = [0, 10, 0]
+pattern = '{0:02d}:{1:02d}:{2:02d}'
+state = False
 
-window.mainloop()
+
+###Functions
+def updateTime():
+    if(state):
+        global timer
+        timer[2] -= 1
+        if timer[2] <= 0:
+            timer[1] -= 1
+            timer[2] = 60
+        if timer[1] <= 0 and timer[2] <= 1:
+            timer[2] = 0
+            pause()
+            blowUp()
+
+    timeString = pattern.format(timer[0], timer[1], timer[2])
+    timeText.configure(text=timeString)
+    root.after(10, updateTime)
+
+def start():
+    global state
+    state = True
+
+def pause():
+    global state
+    state = False
+
+def blowUp():
+    blowUpFrame = tk.Toplevel()
+    blowUpLabel = tk.Label(blowUpFrame, text="KaBooOOoOoOoM!!!", font=("Comic Sans MS", 100))
+    blowUpLabel.pack()
+    blowUpButton = tk.Button(blowUpFrame, text="You're dead!", command=blowUpFrame.destroy)
+    blowUpButton.pack()
+
+###Widgets
+timeText = tk.Label(root, text="00:10:00", font=("Helvetica", 150))
+timeText.pack()
+
+startButton = tk.Button(root, text="Start", command=start)
+startButton.pack()
+
+pauseButton = tk.Button(root, text="Pause", command=pause)
+pauseButton.pack()
+
+quitButton = tk.Button(root, text="Quit", command=root.destroy)
+quitButton.pack()
+
+
+updateTime()
+root.mainloop()
